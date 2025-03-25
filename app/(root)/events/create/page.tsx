3265c9b08a32a4dc7bd57456100103af66d0e20b
Sujeet Gund <sujeetgund@gmail.com>
2025-03-25 18:29:13 +0530

    Fix userId and clerkId problem

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,22 +1,32 @@
-import EventForm from "@/components/shared/EventForm"
+import EventForm from "@/components/shared/EventForm";
+import { getUserId } from "@/lib/actions/user.actions";
 import { auth } from "@clerk/nextjs";
 
-const CreateEvent = () => {
-  const { sessionClaims } = auth();
+const CreateEvent = async () => {
+  const { sessionClaims, userId } = auth();
 
-  const userId = sessionClaims?.userId as string;
+  // const userId = sessionClaims?.userId as string;
+  let dbUserId = undefined;
+  if (userId !== null) {
+    dbUserId = await getUserId(userId);
+    // console.log(dbUserId);
+  }
+
+  if(dbUserId === undefined) return null;
 
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">Create Event</h3>
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Create Event
+        </h3>
       </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Create" />
+        <EventForm userId={dbUserId as string} type="Create" />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default CreateEvent
+export default CreateEvent;
